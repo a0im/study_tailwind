c2b54b767832b1af939642ffad7305e361e1bc50
a0im <https://github.com/a0im>
2022-10-03 21:22:40 +0900

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        1200: "1200px",
+      },
+      height: {
+        30: "7.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
